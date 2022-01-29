8407e223608906f5e0ff5e574a95236a1a2e0fa1
Fahim Chowdhury <88318197+fahim6a@users.noreply.github.com>
2022-01-29 13:33:56 +0600

    Add files via upload

---
+++ Input Variables Debuging.py
@@ -0,0 +1,26 @@
+print("Day-1 string manupulation")
+print('string connection is the done with the "+ " sign.')
+print('e.g print("hello world")') #we can also use single '' for print
+print(('e.g print("hello")'))
+
+input("What is your name")
+#now we are use print and input function together
+print("Hello" + input("what is your name?")+"!")
+
+# variables
+name= input("tell me your name")
+print(name)
+#  we can write another way as like bellow
+name ="Fahim"
+print(name)
+
+# now user given data/name, automatically calculate its length
+# here we use length function for calculating length
+# len()are use for calculate length
+# input() are use for take input from user
+# first name is a variable, which is cotain data when input function take data from user
+# len() calculate data length
+# lets rock&roll
+name = input("Where are you from?")
+length = len(name)
+print(length)
